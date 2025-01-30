09e615f703494e15f5ae5ec2096a3b008fef9528
Matt-33 <matthias.giraudeau@gmail.com>
2025-01-30 16:08:12 +0100

    Modif favorites

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,43 +1,27 @@
 import { useFavoritesStore } from "app/store";
-import { Link } from "react-router-dom";
-import { Movie } from "api/api.props";
-import { useEffect } from "react";
+import MovieCard from "@components/molecules/MovieCard/MovieCard";
 import "./Favorites.css";
 
 export default function Favorites() {
   const { favorites, removeFavorite } = useFavoritesStore();
 
-  useEffect(() => {
-    const storedFavorites = localStorage.getItem("favorites");
-    if (storedFavorites) {
-      const parsedFavorites: Movie[] = JSON.parse(storedFavorites);
-      console.log("Favoris récupérés du localStorage :", parsedFavorites);
-    }
-  }, []);
-
   return (
     <div className="favorites-page">
       <h1>Mes Favoris</h1>
-      <div className="favorites-grid">
-        {favorites.length > 0 ? (
-          favorites.map((movie: Movie, index: number) => (
-            <div className="movie-card" key={`${movie.id}-${index}`}>
-              <Link to={`/movie/${movie.id}`}>
-                <img
-                  src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                  alt={movie.title}
-                />
-              </Link>
-              <h3>{movie.title}</h3>
-              <button onClick={() => removeFavorite(movie)}>
-                Retirer des favoris
-              </button>
-            </div>
-          ))
-        ) : (
-          <p>Aucun film dans vos favoris.</p>
-        )}
-      </div>
+      {favorites.length > 0 ? (
+        <div className="favorites-grid">
+          {favorites.map((movie) => (
+          <MovieCard
+          key={movie.id}
+          movie={movie}
+          isFavorite={true}
+          toggleFavorite={() => removeFavorite(movie.id)} // ✅ On passe uniquement l'ID
+          />        
+          ))}
+        </div>
+      ) : (
+        <p>Aucun film dans vos favoris.</p>
+      )}
     </div>
   );
 }
